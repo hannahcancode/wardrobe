5b34850cc80fb81fe2cbf5fb33aa89faa3f07dfb
Hannah Thompson <thompson.h@gmail.com>
2017-05-13 13:24:59 +1000

    add mongodb and mongoose, update app.js to es6

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const express = require('express');
-const routes = require('./routes/index');
+
+import 'mongodb';
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as routes from './routes/index';
+
+// const db = mongoose.connection;
 
 const app = express();
 
+mongoose.connect('mongodb://localhost/userauthapp');
+
+
 app.use('/', routes);
+
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+// Listen on specified port number
+app.listen(app.get('port'), () => {
+  console.log(`Server started on port ${app.get('port')}`);
+});
